1c6d97392718dd168b3af756f32931a1f94d328d
Bruno <bmeira.bms@gmail.com>
2026-08-25 00:38:17 +0100

    fixes about config service

--- config-server/src/main/java/com/bruno10log/configserver/ConfigController.java
+++ config-server/src/main/java/com/bruno10log/configserver/ConfigController.java
@@ -1,6 +1,10 @@
+package com.bruno10log.configserver;
+
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,11 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/config")
 @RefreshScope
 public class ConfigController {
-    
+
+    @Value("${custom.message:}")
     private String message;
 
+    @Value("${logging.level.root:}")
     private String logLevel;
 
+    @GetMapping
     public Map<String, String> getConfig() {
         return Map.of("message", message, "logLevel", logLevel);
     }
